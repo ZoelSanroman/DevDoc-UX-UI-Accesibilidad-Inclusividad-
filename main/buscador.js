2b1fd266886d1207d824e12c84f35763438fbed2
ZoelSanroman <zoelmarfonfog@gmail.com>
2024-05-30 09:43:17 +0200

    add imports and fix bugs

---
+++ main/buscador.js
@@ -0,0 +1,29 @@
+export function initBuscador() {
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form[role="search"]');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const searchText = document.getElementById('buscador').value.toLowerCase();
+        const articles = document.querySelectorAll('.lead, .Divergencias h2.fw-normal');
+        let found = false;
+
+        articles.forEach(article => {
+            if (article.textContent.toLowerCase().includes(searchText)) {
+                console.log('Texto encontrado en el artículo:', article.textContent);
+                if (article.closest('.Divergencias')) {
+                    article.closest('.Divergencias').scrollIntoView({ behavior: 'smooth' });
+                    found = true;
+                } else {
+                    article.scrollIntoView({ behavior: 'smooth' });
+                    found = true;
+                }
+            }
+        });
+
+        if (!found) {
+            alert('No se encontró el artículo deseado. Puede pedirnoslo como sugerencia, consulte: contacto.');
+        }
+    });
+});
+}
--- main/main.js
+++ main/main.js
@@ -1,30 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form[role="search"]');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+import { initBuscador } from './buscador.js';
 
-        const searchText = document.getElementById('buscador').value.toLowerCase();
-        const articles = document.querySelectorAll('.lead, .Divergencias h2.fw-normal');
-        let found = false;
-
-        articles.forEach(article => {
-            if (article.textContent.toLowerCase().includes(searchText)) {
-                console.log('Texto encontrado en el artículo:', article.textContent);
-                if (article.closest('.Divergencias')) {
-                    article.closest('.Divergencias').scrollIntoView({ behavior: 'smooth' });
-                    found = true;
-                } else {
-                    article.scrollIntoView({ behavior: 'smooth' });
-                    found = true;
-                }
-            }
-        });
-
-        if (!found) {
-            alert('No se encontró el artículo deseado. Puede pedirnoslo como sugerencia, consulte: contacto.');
-        }
-    });
-});
+initBuscador();
 
 const sonidito = 'sonidito.mp3';
 const clickar = new Audio(sonidito);
